4b540fea78b7b1d8f82ccd81fbd21b63048f5bb1
Justin Guerré <guerrejustin@gmail.com>
2022-07-21 22:04:13 +0200

    fix flicker

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,10 +3,10 @@ import Navbar from "./Navbar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <div className="dark:bg-gray-800">
       <Navbar />
       <main className="pt-20 mx-auto max-w-5xl">{children}</main>
       <Footer />
-    </>
+    </div>
   );
 }
